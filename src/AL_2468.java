5ab6e01886f6352c4444b7a9d72308535739a516
Bigfish4tim <bigfish.4tim@gmail.com>
2022-11-02 13:14:06 +0900

    11-02

---
+++ src/AL_2468.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class AL_2468 {
+    static int n;
+    static int[][] map;
+    static int max=0;
+    static int[] dx = { -1, 1, 0, 0 };
+    static int[] dy = { 0, 0, -1, 1 };
+    static boolean[][] visit;
+
+    public static class Operator {
+        int x;
+        int y;
+        public Operator(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static int bfs(int[][] pmap, int h, boolean[][] visited) {
+        Queue<Operator> q = new LinkedList<>();
+
+        int total = 0;
+
+        for(int i=0; i<pmap.length; i++) {
+            for(int j=0; j< pmap.length; j++) {
+                if(pmap[i][j] > h && !visited[i][j]) {
+                    q.offer(new Operator(i, j));
+                    visited[i][j] = true;
+                    total++;
+                }
+                while (!q.isEmpty()) {
+                    Operator temp = q.poll();
+                    for(int k=0; k<4; k++) {
+                        int nextX = temp.x + dx[k];
+                        int nextY = temp.y + dy[k];
+
+                        if(nextX < 0 || nextX >= pmap.length || nextY < 0 || nextY >= pmap.length || pmap[nextX][nextY] <= h || visited[nextX][nextY]) {
+                            continue;
+                        }
+                        q.offer(new Operator(nextX, nextY));
+                        visited[nextX][nextY] = true;
+                    }
+                }
+            }
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        visit = new boolean[n][n];
+
+        int maxh = 0;
+
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(maxh < map[i][j]) {
+                    maxh = map[i][j];
+                }
+            }
+        }
+
+        for(int i=1; i<maxh; i++) {
+            int h = bfs(map, i, visit);
+
+            if(max < h)
+                max = h;
+
+            visit = new boolean[n][n];
+        }
+
+        System.out.println(max);
+    }
+}
