b8c7f3412f2567adf7cc5039216b5a451d0fbc1b
Bigfish4tim <bigfish.4tim@gmail.com>
2022-11-16 17:04:21 +0900

    11-16

--- src/AL_17071.java
+++ src/AL_17071.java
@@ -6,27 +6,40 @@ import java.util.*;
 public class AL_17071 {
     static int n;
     static int k;
-    static int[] visit = new int[500001];
+    static int[][] visit = new int[500001][2];
+    static boolean[][] visited = new boolean[500001][2];
+
+    public static class Operator {
+        int x;
+        int count;
+
+        public Operator(int x, int count) {
+            this.x = x;
+            this.count = count;
+        }
+    }
+
+    public static int arithmetic(int n) {
+        return n*(n+1)/2;
+    }
 
     public static void bfs(int start, int end) {
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Operator> oq = new LinkedList<>();
 
-        visit[start] = 1;
-
-        int Kcount = 0;
-        int Kcoe = 1;
+        visited[start][1] = true;
 
         if(start==end) {
             System.out.println(0);
             return;
         }
+        oq.offer(new Operator(start, 1));
 
-        q.offer(start);
+        while (!oq.isEmpty()) {
+            Operator tempq = oq.poll();
 
-        while (!q.isEmpty()) {
-            int temp = q.poll();
-            end = end + Kcoe;
-            Kcoe++;
+            int temp = tempq.x;
+
+            int odds = tempq.count % 2;
 
             for(int i=0; i<3; i++) {
                 int next;
@@ -39,17 +52,23 @@ public class AL_17071 {
                     next = temp*2;
                 }
 
-                if(next==end) {
-                    System.out.println(visit[temp]);
+                if(end+arithmetic(tempq.count) > 500000) {
+                    System.out.println(-1);
                     return;
                 }
 
-                if(next >= 0 && next < visit.length && visit[next] == 0) {
-                    q.offer(next);
-                    visit[next] = visit[temp] + 1;
+                if(visited[end+arithmetic(tempq.count)][odds]) {
+                    System.out.println(tempq.count);
+                    return;
+                }
+
+                if(next >= 0 && next < visit.length && !visited[next][odds]) {
+                    visited[next][odds] = true;
+                    oq.offer(new Operator(next, tempq.count+1));
                 }
             }
         }
+        System.out.println(-1);
     }
 
     public static void main(String[] args) throws IOException {
