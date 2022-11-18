4f98cd898e27e045942546666307ba26dadb2833
Bigfish4tim <bigfish.4tim@gmail.com>
2022-11-18 17:04:07 +0900

    11-18

--- src/AL_3197.java
+++ src/AL_3197.java
@@ -6,11 +6,83 @@ import java.util.*;
 public class AL_3197 {
     static int R, C;
     static char[][] map;
+    static int[][] Lpoints = new int[2][2];
+
+    static Queue<int[]> StartPoint = new LinkedList<>();
+
+    static boolean[][] Lastvisit;
+    static boolean[][] visit;
+
+    static boolean isFind;
+
+    static int ans=0;
+
+    static Queue<int[]> xq = new LinkedList<>();
+    static Queue<int[]> xs = new LinkedList<>();
+
     static int[] dx = new int[] { -1, 1, 0, 0 };
     static int[] dy = new int[] { 0, 0, -1, 1 };
 
-    public static void bfs() {
+    public static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
 
+        q.offer(new int[] {x, y});
+        visit[x][y] = true;
+
+        while (!q.isEmpty()) {
+            int[] temp = q.poll();
+            for(int i=0; i<4; i++) {
+                int nextX = temp[0] + dx[i];
+                int nextY = temp[1] + dy[i];
+
+                if(nextX < 0 || nextX >= R || nextY < 0 || nextY >= C)
+                    continue;
+                if(visit[nextX][nextY])
+                    continue;
+                if(map[nextX][nextY] == 'X') {
+                    StartPoint.offer(new int[] {temp[0], temp[1]});
+                    continue;
+                }
+                if (nextX == Lpoints[1][0] && nextY == Lpoints[1][1]) {
+                    isFind = true;
+                    break;
+                }
+                q.offer(new int[] {nextX, nextY});
+                visit[nextX][nextY] = true;
+            }
+
+        }
+    }
+
+    public static void findx() {
+        int size = xq.size();
+        for(int j=0; j<size; j++) {
+            int[] temp = xq.poll();
+
+            for(int i=0; i<4; i++) {
+                assert temp != null;
+                int nextX = temp[0] + dx[i];
+                int nextY = temp[1] + dy[i];
+
+                if(nextX < 0 || nextX >= R || nextY < 0 || nextY >= C) {
+                    if(i==3)
+                        xq.offer(temp);
+                    continue;
+                }
+                if(map[nextX][nextY] == 'X') {
+                    if(i==3)
+                        xq.offer(temp);
+                    continue;
+                }
+                xs.offer(temp);
+                break;
+            }
+        }
+
+        while (!xs.isEmpty()) {
+            int[] temp = xs.poll();
+            map[temp[0]][temp[1]] = '.';
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -22,26 +94,36 @@ public class AL_3197 {
         C = Integer.parseInt(st.nextToken());
 
         map = new char[R][C];
+        Lastvisit = new boolean[R][C];
+        visit = new boolean[R][C];
 
-//        for(int i=0; i<R; i++) {
-//            st = new StringTokenizer(br.readLine());
-//            for(int j=0; j<C; j++) {
-//                map[i][j] = st.nextToken();
-//            }
-//        }
-
+        int Lcount = 0;
         for(int i = 0 ; i < R ; ++i) {
             char[] line = br.readLine().toCharArray();
             for(int j = 0 ; j < C ; ++j) {
                 map[i][j] = line[j];
-//                if(map[i][j] == 'L') {
-//                    swan[swanIdx++] = new Node(r, c);
-//                }
-//                if(map[r][c] != 'X') {
-//                    waterQ.offer(new Node(r, c));
-//                }
+                if(map[i][j] == 'L') {
+                    Lpoints[Lcount][0] = i;
+                    Lpoints[Lcount][1] = j;
+                    Lcount++;
+                }
+                if(map[i][j] == 'X') {
+                    xq.offer(new int[] {i, j});
+                }
             }
         }
+        StartPoint.offer(new int[] {Lpoints[0][0], Lpoints[0][1]});
 
+        while (!StartPoint.isEmpty()) {
+            visit = new boolean[R][C];
+            int[] starter = StartPoint.poll();
+            bfs(starter[0], starter[1]);
+            if(isFind)
+                break;
+            findx();
+            ans++;
+        }
+
+        System.out.println(ans);
     }
 }
